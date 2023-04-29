a05dd146b70fa8aa6fa0095a9e15a03fa4809bd4
DAN_WEI ZUO <danweizuo@gmail.com>
2023-04-29 16:20:00 -0400

    working on linear regression

--- linear_regression.py
+++ linear_regression.py
@@ -5,31 +5,32 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 
-class LinearRegression(torch.nn.Module):
-   
-   def preprocessing(self):
-        df = pd.read_csv('data\Immunisations.csv')
-        to_drop = ['VAR','UNIT','Measure','COU','YEA', 'Flag Codes','Flags']
-        df.drop(to_drop, inplace=True, axis=1)
-        #creating a separate dataframe with just the doctors  
-        df_doc = pd.read_csv('data\Physicians.csv')
-        to_drop = ['VAR','UNIT','Measure','COU','YEA', 'Flag Codes','Flags']
-        df_doc.drop(to_drop, inplace=True, axis=1)
-        df_doc = df_doc[df_doc['Variable'].str.contains('Practising physicians')]
-        merged = pd.merge(left=df, right=df_doc, how='right', left_on=['Year','Country'], right_on=['Year','Country'])
-        merged = merged.rename(columns={'Value_x': 'percent_immun', 'Value_y': 'num_physicians'})
-        LinearRegression.split(merged)
+#preprocessing
+df = pd.read_csv('data\Immunisations.csv')
+to_drop = ['VAR','UNIT','Measure','COU','YEA', 'Flag Codes','Flags']
+df.drop(to_drop, inplace=True, axis=1)
 
-   def split(self, df):
-        merged = df
-        train_size = int(0.8 * len(merged))
-        test_size = len(merged) - train_size
-        train_dataset, test_dataset = torch.utils.data.random_split(merged, [train_size, test_size])
+#creating a separate dataframe with just the doctors  
+df_doc = pd.read_csv('data\Physicians.csv')
+to_drop = ['VAR','UNIT','Measure','COU','YEA', 'Flag Codes','Flags']
+df_doc.drop(to_drop, inplace=True, axis=1)
+df_doc = df_doc[df_doc['Variable'].str.contains('Practising physicians')]
+merged = pd.merge(left=df, right=df_doc, how='right', left_on=['Year','Country'], right_on=['Year','Country'])
+merged = merged.rename(columns={'Value_x': 'percent_immun', 'Value_y': 'num_physicians'})
+
+#split data
+
+#number of features to pass into model
+num_features = len(merged.columns())
+
+class LinearRegression(torch.nn.Module):
 
    def __init__(self):
         super(LinearRegression, self).__init__() 
-        self.linear = nn.Linear(1, 1)
+        self.linear = nn.Linear(num_features, 1)
       
    def forward(self, x): 
         predict_y = self.linear(x) 
         return predict_y
+   
+model = LinearRegression(num_features, )
